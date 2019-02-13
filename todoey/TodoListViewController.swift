0e798826ff04a12090d7874c6233c92dcc81e375
imWearingPJs <michaelkozub@gmail.com>
2019-02-13 10:16:05 -0700

    add items and reload the tableview

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Camping", "Shopping", "Upcomming Trip"]
+    var itemArray = ["Camping", "Shopping", "Upcomming Trip"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,28 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //what happens when user clicks add item
+            print("success")
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
